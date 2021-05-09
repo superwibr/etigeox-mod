45a2b5a230de7a23d0386bf444984eb4e788cb97
superwibr <65673005+superwibr@users.noreply.github.com>
2021-05-09 18:30:51 -0400

    added buildablke core

--- scripts/blocks/scrapwalls.js
+++ scripts/blocks/scrapwalls.js
@@ -3,3 +3,6 @@ Blocks.scrapWall.buildVisibility = BuildVisibility.shown;
 Blocks.scrapWallLarge.buildVisibility = BuildVisibility.shown;
 Blocks.scrapWallHuge.buildVisibility = BuildVisibility.shown;
 Blocks.scrapWallGigantic.buildVisibility = BuildVisibility.shown;
+
+// buildable shard
+Blocks.coreShard.buildVisibility = BuildVisibility.shown;
