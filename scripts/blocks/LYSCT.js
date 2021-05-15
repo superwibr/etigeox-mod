631fdedbbab439ee97c50e839c49e99f2328d240
superwibr <superwibr@gmail.com>
2021-05-15 09:20:55 -0400

    ???

--- scripts/blocks/LYSCT.js
+++ scripts/blocks/LYSCT.js
@@ -3,7 +3,7 @@
 
 
 // Recycler item selection
-const recycler = extend(GenericCrafter, "recycler", {})
+const recycler = extend(LiquidConverter, "recycler", {})
 recycler.buildType = () => extend(Building, {
     buildConfiguration(table){
         table.button(Items.metaglass.icon(Cicon.medium), Styles.clearTransi, () => { recycler.outputItem = {item: Items.metaglass, amount: 1} });
