238a7781253ca5c59910beb21f8151d6f26c1701
superwibr <superwibr@gmail.com>
2021-05-16 14:31:35 -0400

    error?

--- scripts/blocks/LYSCT.js
+++ scripts/blocks/LYSCT.js
@@ -3,7 +3,7 @@
 
 
 // Recycler item selection
-const recycler = extend(GenericCrafter, "recycler")
+const recycler = extend(GenericCrafter, "recycler", {})
 recycler.buildType = () => extend(GenericCrafter.GenericCrafterBuild, recycler, {
     buildConfiguration(table){
         table.button(Items.metaglass.icon(Cicon.medium), Styles.clearTransi, () => recycler.outputItem = {item: Items.metaglass, amount: 1} );
