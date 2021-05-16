a6d313d665ba600c6dc186f9bd140bab9a1b91e0
superwibr <superwibr@gmail.com>
2021-05-16 14:37:45 -0400

    last thing but yes

--- scripts/blocks/LYSCT.js
+++ scripts/blocks/LYSCT.js
@@ -7,7 +7,7 @@ const recycler = extend(GenericCrafter, "recycler", {})
 recycler.buildType = () => extend(GenericCrafter.GenericCrafterBuild, recycler, {
     buildConfiguration(table){
         let button = item => {
-           return `table.button(new TextureRegionDrawable(Items.${item}.icon(Cicon.medium)), Styles.clearTransi, () => recycler.outputItem = new ItemStack(Items.${item}, 1));`
+           eval(`table.button(new TextureRegionDrawable(Items.${item}.icon(Cicon.medium)), Styles.clearTransi, () => recycler.outputItem = new ItemStack(Items.${item}, 1));`)
         }
         button('metaglass')
     }
