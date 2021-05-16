d44dd86a3b00e51070ea07919aafa15d67863808
superwibr <superwibr@gmail.com>
2021-05-16 14:50:44 -0400

    itembuttoun

--- scripts/blocks/LYSCT.js
+++ scripts/blocks/LYSCT.js
@@ -7,8 +7,12 @@ const recycler = extend(GenericCrafter, "recycler", {})
 recycler.buildType = () => extend(GenericCrafter.GenericCrafterBuild, recycler, {
     buildConfiguration(table){
         let button = item => {
-           eval(`table.button(new TextureRegionDrawable(Items.${item}.icon(Cicon.medium)), Styles.clearTransi, () => recycler.outputItem = new ItemStack(Items.${item}, 1));`)
+           table.button(new TextureRegionDrawable(Items[item].icon(Cicon.medium)), Styles.clearTransi, () => recycler.outputItem = new ItemStack(Items[item], 1));
         }
         button('metaglass')
+        button('surge-alloy')
+        button('plastanium')
+        button('blast-compound')
+        button('pyratite')
     }
 });
