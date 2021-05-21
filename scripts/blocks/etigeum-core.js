96893627abcdad80ff93814638211f7f9d50c479
superwibr <superwibr@gmail.com>
2021-05-21 08:06:53 -0400

    ALMOST THERE....

--- scripts/blocks/etigeum-core.js
+++ scripts/blocks/etigeum-core.js
@@ -1,15 +1,21 @@
 // core.
 // it makes power.
 
-const etgcore = extend(BurnerGenerator, "etigeum-core", {})
+const etgcore = extend(BurnerGenerator, "etigeum-core", {
+    update: true
+})
 etgcore.buildType = () => extend(BurnerGenerator.BurnerGeneratorBuild, etgcore, {
     updateTile(){   
-        Vars.ui.showInfoToast("toast", 0.9)
         this.super$updateTile()
 
-        if(this.block.generateTime >= 1){
-            Vars.ui.showInfoToast("core prod > 1", 0.9)
-            //this.dump(this.block.outputItem.item);
+        //Vars.ui.showInfoToast("prod: "+this.productionEfficiency+",  prodt: "+this.generateTime.toString().substring(0, 5), 1)
+
+        if(this.generateTime <= 0){
+            Vars.ui.showInfoToast("core prod!", 1)
+            this.dump(/*this.block.outputItem.item*/);
+            for(let i = 0; i < 2 /*this.block.outputItem.amount = 2*/; i++){
+                this.offload(/*this.block.outputItem.item = depleted-etigeum*/);
+            }
         }
     }
 });
