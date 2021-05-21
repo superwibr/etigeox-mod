1b6977b0407d44bfbbed1f7e19fec4573a539e48
superwibr <superwibr@gmail.com>
2021-05-21 11:05:04 -0400

    cleanup

--- scripts/blocks/etigeum-core.js
+++ scripts/blocks/etigeum-core.js
@@ -7,12 +7,9 @@ const etgcore = extend(BurnerGenerator, "etigeum-core", {
 etgcore.buildType = () => extend(BurnerGenerator.BurnerGeneratorBuild, etgcore, {
     updateTile(){   
         this.super$updateTile()
-
-        //Vars.ui.showInfoToast("prod: "+this.productionEfficiency+",  prodt: "+this.generateTime.toString().substring(0, 5), 1)
-
         if(this.generateTime >= 0.1 && this.generateTime <= 0.12){
-            for(let i = 0; i < 2 /*this.block.outputItem.amount = 2*/; i++){
-                this.offload(Vars.content.getByName(ContentType.item, "etigeox-etigeum-depleted")/*this.block.outputItem.item = depleted-etigeum*/);
+            for(let i = 0; i < 2; i++){
+                this.offload(Vars.content.getByName(ContentType.item, "etigeox-etigeum-depleted"));
             }
         }
     }
